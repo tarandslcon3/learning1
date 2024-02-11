12d1fea64a4812a2b8666e04d5a68f9cdd70a365
tarandslcon3@gmail.com <tarandslcon3@gmail.com>
2024-02-11 11:18:30 -0500

    new

---
+++ main.py
@@ -0,0 +1,12 @@
+x = int(input("wahts your number: "))
+fruits =['apple', 'pear',x ]
+print (fruits)
+fruits.append('yooo4234234')
+print (fruits)
+y = 5
+#x = fruits[2]
+
+if x+y == 6:
+    print ('no')
+else:
+    print ('NOOOOOOOOOOOOOOO')
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+x = int(input("wahts your number: "))
+fruits =['apple', 'pear',x ]
+y = 5
+#x = fruits[2]
+
+if x+y == 6:
+    print ('no')
+else:
+    print ('NOOOOOOOOOOOOOOO')
